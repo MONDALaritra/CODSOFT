e35ef3779d495e08c5145d3c86b7936d1ef646fd
Aritra Mondal <159824625+MONDALaritra@users.noreply.github.com>
2024-05-20 21:18:05 +0530

    Update Project1.java

--- JAVA_INTERNSHIPS/Project1.java
+++ JAVA_INTERNSHIPS/Project1.java
@@ -6,10 +6,13 @@ class Project1{
         Scanner sc=new Scanner(System.in);
         Project1 p=new Project1();
         int wish;
+        System.out.println("TOTAL NUMBER OF ATTEMPTS YOU CAN PERFORM IS 3");
+        System.out.println(">>>>>>SCORE RULE<<<<<< ");
+        System.out.println("SUCCESSFULL IN FIRST ATTEMPT:100 POINTS \n SUCCESSFULL IN SECOND ATTEMPT:50 POINTS \n SUCCESSFULL IN THIRD ATTEMP:25 POINTS");
         do{
-            System.out.println("TOTAL NUMBER OF ATTEMPTS YOU CAN PERFORM IS 3");
+            
             p.RandomGen();
-            System.out.print("DO YOU WANT TO CONTINUE? PRESS 1 TO CONTINUE : ");
+            System.out.print("DO YOU WANT TO CONTINUE THE GAME? PRESS 1 TO CONTINUE : ");
             wish=sc.nextInt();
             
         }while(wish==1);
@@ -17,11 +20,12 @@ class Project1{
 
     public void RandomGen(){
         Scanner sc=new Scanner(System.in);
+        int max=100;
+        int min=1;
+        int range= (max-min)+1;
+        int rand= (int)(Math.random() * range) + min;
         for(int i=1;i<=3;i++){
-            int max=100;
-            int min=1;
-            int range= (max-min)+1;
-            int rand= (int)(Math.random() * range) + min;
+            
             int score=0;
             System.out.println("......ATTEMPT "+i+".....");
             System.out.print("GUESS THE NUMBER BETWEEN 1 TO 100:");
@@ -33,7 +37,17 @@ class Project1{
             if(num>=1 && num<=100){
                 if(num==rand){
                     System.out.println("Its Correct....");
-                    score++;
+                    if(i==1){
+                        System.out.println("EXCELLENT, YOUR SCORE IS 100.");
+                    }
+                    else if(i==2){
+                        System.out.println("GOOD, YOUR SCORE IS 50.");
+                    }
+                    else{
+                        System.out.println("NOT BAD, YOUR SCORE IS 25.");
+                    }
+                    
+                    break;
                 }
                 else if(num>rand){
                     System.out.println("Too High");
@@ -46,7 +60,7 @@ class Project1{
                 }
                 catch(InterruptedException e){}
                 System.out.println("The actual number is:"+rand);
-                System.out.println("YOUR SCORE IS "+score+" OUT OF 3");
+                
             }
             else{
                 try{
